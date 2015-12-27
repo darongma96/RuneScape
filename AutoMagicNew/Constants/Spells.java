2fff45d09e1a9e21f9814fca59a9cb4eb19d6b36
darongma96 <darongma96@gmail.com>
2015-12-27 18:58:09 -0500

    Create Spells.java

---
+++ AutoMagicNew/Constants/Spells.java
@@ -0,0 +1,13 @@
+package scripts.AutoMagic.Constants;
+
+import org.tribot.api2007.Player;
+import org.tribot.api2007.types.RSTile;
+
+public class Spells {
+	public static String spellToCast;
+	public static String npcToCast;
+	public final static RSTile startingTile = Player.getPosition();
+	public static String itemToAlch;
+	public static boolean run_script;
+	public static String mode;
+}
